7b4d4197aec1a5f52f58bb63419e720c3bfe64ae
kaminokazuya <zeal24360186@fsgcl.jp>
2025-05-20 12:18:12 +0900

    RectTestの追加

---
+++ src/デフォルト/RectTest.java
@@ -0,0 +1,8 @@
+package デフォルト;
+
+public class RectTest extends MyFrame {
+		public void run() {
+			fillRect(30,50,200,100);
+		}
+
+}
--- src/デフォルト/Start.java
+++ src/デフォルト/Start.java
@@ -4,7 +4,8 @@ package デフォルト;
 public class Start {
 
 	public static void main(String[] args) {
-		System.out.println("ドアホ");
+		System.out.println("プログラムを実行します");
+		MyFrame frame1=new RectTest();
 	}
 
 }
