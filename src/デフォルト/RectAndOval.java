870241d3866f4a5309a9f7c06c0ee360987028cd
kaminokazuya <zeal24360186@fsgcl.jp>
2025-05-26 11:18:21 +0900

    RectAndOvalの追加、車のおもちゃの絵を描画

---
+++ src/デフォルト/RectAndOval.java
@@ -0,0 +1,12 @@
+package デフォルト;
+
+public class RectAndOval extends MyFrame {
+
+	public void run() {
+		fillRect(50,100,50,30);
+		fillRect(30,120,90,20);
+		fillOval(45,140,20,20);
+		fillOval(85,140,20,20);
+	}
+
+}
