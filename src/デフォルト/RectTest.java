6b3ad41b121d9094dada51ec016f1e6c86bbc376
kaminokazuya <zeal24360186@fsgcl.jp>
2025-05-26 10:05:13 +0900

    四角を追加し、色を追加

--- src/デフォルト/RectTest.java
+++ src/デフォルト/RectTest.java
@@ -2,7 +2,18 @@ package デフォルト;
 
 public class RectTest extends MyFrame {
 		public void run() {
-			fillRect(30,50,200,100);
+			setColor(255,255,0);
+			fillRect(30,40,50,100);
+			setColor(200,0,255);
+			fillRect(130,40,50,100);
+			setColor(0,200,255);
+			fillRect(30,200,150,10);
+			setColor(255,255,255);
+			fillRect(30,100,150,10);
+			setColor(150,150,150);
+			fillRect(30,300,150,10);
+			setColor(255,255,255);
+			fillRect(30,350,150,10);
 		}
 
 }
