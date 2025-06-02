93fc2c904a53a46635d4651e136e6bb0c0448594
kaminokazuya <zeal24360186@fsgcl.jp>
2025-06-02 11:47:49 +0900

    DisplayFace Face DisplayFaceObjectの追加

---
+++ src/デフォルト/DisplayFaceObject.java
@@ -0,0 +1,16 @@
+package デフォルト;
+
+public class DisplayFaceObject extends MyFrame {
+
+	public void run() {
+		Face face1=new Face(50,50);
+		face1.draw(this);
+		
+		Face face2=new Face(250,50);
+		face2.draw(this);
+		
+		Face face3=new Face(150,150);
+		face3.draw(this);
+	}
+
+}
